1bb6a60b31e7c8d0e8f3ce37927bb02f0cbc21e3
Mario E. Perez F <90684949+DaddyShortMember@users.noreply.github.com>
2022-03-28 19:16:27 +0200

    Add files via upload

---
+++ menuWindow.c
@@ -0,0 +1,24 @@
+/*
+ * menuWindow.c
+ *
+ *  Created on: 26 Mar 2022
+ *      Author: mario
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+printf("Online Banking Application (O.B.A.)\n");
+printf("BUILD Dev-0\n");
+printf("Bank Teller Client\n");
+printf("Press [ENTER] to log in.");
+puts("\n");
+getch();
+system("CLS");
+printf("Select choice:\n");
+printf("1. Review Loan Applications\n");
+printf("2. Review Account-related Requests\n");
+printf("3. Log Out and Exit\n");
+scanf();
+}
