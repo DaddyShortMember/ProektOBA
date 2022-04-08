454610a950e309e130a66c1b855ac63d663a2115
PabloDullaghan <90982579+PabloDullaghan@users.noreply.github.com>
2022-04-08 11:45:43 +0200

    Add files via upload

--- adminClient/menuWindow.c
+++ adminClient/menuWindow.c
@@ -5,20 +5,35 @@
  *      Author: mario
  */
 
+#include "fnc/reviewer.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(){
-printf("Online Banking Application (O.B.A.)\n");
-printf("BUILD Dev-0\n");
-printf("Bank Teller Client\n");
-printf("Press [ENTER] to log in.");
-puts("\n");
-getch();
-system("CLS");
-printf("Select choice:\n");
-printf("1. Review Loan Applications\n");
-printf("2. Review Account-related Requests\n");
-printf("3. Log Out and Exit\n");
-scanf();
+	int opcion;
+	printf("Online Banking Application (O.B.A.)\n");
+	printf("BUILD Dev-0\n");
+	printf("Bank Teller Client\n");
+	printf("Press [ENTER] to log in.");
+	puts("\n");
+	getch();
+	system("CLS");
+	printf("Select choice:\n");
+	printf("1. Review Loan Applications\n");
+	printf("2. Review Account-related Requests\n");
+	printf("3. Log Out and Exit\n");
+	scanf(%d, &opcion);
+	switch (opcion){
+		case 1:
+			print(getList();
+			break;
+		case 2:
+			
+			break;
+		case 3:
+			
+			break;
+		default:
+			break;
+	}
 }
