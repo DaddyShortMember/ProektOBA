007c0a7996c47dbd581cbf11725f7264045d5e76
Mario E. Perez F <90684949+DaddyShortMember@users.noreply.github.com>
2022-05-04 16:03:44 +0200

    Delete db directory

--- db/queryMngr.h
+++
@@ -1,8 +0,0 @@
-#ifndef QUERYMNGR_H_
-#define QUERYMNGR_H_
-
-void respondLoan(char[] NIF, char response);
-void respondNewAcc(char[] NIF, char response);
-void respondDeleteAcc(char[] NIF, char response);
-
-#endif
